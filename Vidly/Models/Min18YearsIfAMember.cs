7b10e1c56e5e07e9f4b0317c38770b62d6c7e660
Panos <anastapd@gmail.com>
2017-11-20 12:37:25 +0000

    ImplementBirthDateValidationForCustomer

---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vidly.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId==0||customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+            if (customer.BirthDate == null)
+                return new ValidationResult("Birthdate is required.");
+
+            var age = DateTime.Now.Year - customer.BirthDate.Value.Year;
+
+            return (age >=18) 
+                ? ValidationResult.Success 
+                : new ValidationResult("You hould be at least 18 years old to go on a membership type.");
+        }
+    }
+}
